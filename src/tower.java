cdde9dcffb44997848f394398b3598a9a77af7f3
Kaolyfin <dosaikun@gmail.com>
2018-05-08 14:41:34 +0200

    Tower class

---
+++ src/tower.java
@@ -0,0 +1,12 @@
+public class Tower {
+
+    private double pos;
+    private double attack;
+    private double hp;
+
+    public Tower(double pos, double attack, double hp) {
+        this.pos=pos;
+        this.attack=attack;
+        this.hp=hp;
+    }
+}
