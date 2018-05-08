9cf94b1487c8da1c0bf9b0cbe909e50bef06db35
Earl Fluffy <milena.kaag@insa-lyon.fr>
2018-05-08 15:56:10 +0200

    Map

--- src/MapConfig.java
+++ src/MapConfig.java
@@ -1,5 +1,23 @@
 public class MapConfig {
+
+    private double[][] infosTours1= {{1,1,100,2000},{10,6,100,2000}};
+    Tower[] tours1;
+
     public MapConfig(){
-        Map map1 = new Map();
+
+        towerConfig();
+
+        Map map1 = new Map("Grille", tours1 );
     }
+
+    private void towerConfig(){
+        for(int i=0;i<infosTours1.length;i++) {
+            Tower tour = new Tower(infosTours1[i][0], infosTours1[i][1], infosTours1[i][2], infosTours1[i][3]);
+            tours1[i]=tour;
+
+        }
+
+    }
+
+
 }
