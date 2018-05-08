22c752af8a735c0d99319842cb06a9b868a7439d
Rader <blob1408@outlook.com>
2018-05-08 14:11:37 +0200

    graphiccommit

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,11 @@
-import com.sun.scenario.effect.light.SpotLight;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.event.Event;
-import javafx.event.EventHandler;
-import javafx.geometry.Point3D;
-import javafx.scene.*;
-import javafx.scene.image.Image;
-import javafx.scene.input.KeyCode;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.PhongMaterial;
-import javafx.scene.shape.*;
-import javafx.scene.transform.Rotate;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.stage.Stage;
 
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseEvent;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 import static javafx.beans.binding.Bindings.select;
 public class Main extends Application {
@@ -25,23 +13,46 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         System.out.println("If you see this then you got githb to work");
+        Application.launch();
     }
 
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Soo What now?");
+
+        Group root = new Group();
+        Canvas canvas = new Canvas(800,600);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        root.getChildren().add(canvas);
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
+
 
         lastNanoTime = System.nanoTime();
         AnimationTimer animator = new AnimationTimer(){
 
             @Override
             public void handle(long arg0) {
+                double cTime = (arg0- lastNanoTime)/10000;
+                lastNanoTime = arg0;
 
+                logic(cTime);
+
+                render(gc);
             }
         };
 
         animator.start();
+    }
+
+
+    public void logic(double loopTime){
+
+    }
+
+    public void render(GraphicsContext displayGraphics){
+        displayGraphics.fillRect(10,10,80,50);
     }
 }
 
